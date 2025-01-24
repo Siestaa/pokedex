8e43f229042f7dd086785324a3fa9e50b5566cdc
Siestaa <lex.qz97@gmail.com>
2025-01-24 16:12:08 +0500

    fix deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	output: 'export', // <=== enables static exports
 	reactStrictMode: true,
 	images: {
 		domains: ['www.pokemon.com'],
